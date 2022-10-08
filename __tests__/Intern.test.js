e839bf190ba03e822f86b5fb145c2745558ce940
tmunkhb <tmunkhb@gmail.com>
2022-10-08 15:29:04 -0400

    add intern class/test

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,29 @@
 const Intern = require("../lib/Intern");
+const intern = new Intern ('Lee', '1234', 'leetest@gmail.com', 'GMU');
+
+test('creates a intern object via employee constructor', () => {
+    expect(intern.name).toBe('Lee');
+    expect(intern.id).toBe('1234');
+    expect(intern.email).toBe('leetest@gmail.com');
+    expect(intern.school).toBe('GMU');
+});
+
+test('testing getName method', () => {
+    expect(intern.getName()).toBe('Lee');
+});
+
+test('testing getId method', () => {
+    expect(intern.getId()).toBe('1234')
+});
+
+test('testing getEmail method', () => {
+    expect(intern.getEmail()).toBe('leetest@gmail.com')
+});
+
+test('testing getSchool method', () => {
+    expect(intern.getSchool()).toBe('GMU')
+});
+
+test('testing getRole method', () => {
+    expect(intern.getRole()).toBe('Intern')
+});
